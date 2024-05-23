df2d003f4de60472e2ba43ae57f18f1d2814f3d0
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2024-05-23 09:12:04 +0700

    feat: complete chapter 1 docker sql

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -1,78 +1,88 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-import os
-import argparse
-
+# Cleaned up version of data-loading.ipynb
+import argparse, os, sys
 from time import time
-
-import pandas as pd
+import pyarrow.parquet as pq
 from sqlalchemy import create_engine
 
 
-def main(params):
+def main(params: argparse.Namespace):
     user = params.user
     password = params.password
-    host = params.host 
-    port = params.port 
+    host = params.host
+    port = params.port
     db = params.db
-    table_name = params.table_name
+    table_name = params.tb
     url = params.url
-    
-    # the backup files are gzipped, and it's important to keep the correct extension
-    # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
+    batch_count = params.batch_count
+    batch_size = 100000 or params.batch_size
+
+    # Get the name of the file from url
+    file_name = url.rsplit("/", 1)[-1].strip()
+    if os.path.exists(file_name):
+        print(f"{file_name} already exists. Skipping download.")
     else:
-        csv_name = 'output.csv'
+        # Download file from url
+        print(f"Downloading {file_name} ...")
+        os.system(f"wget -O {file_name} {url.strip()}")
+    print("\n")
 
-    os.system(f"wget {url} -O {csv_name}")
+    # Create SQL engine
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    # Read file
+    if file_name.endswith(".parquet"):
+        file = pq.ParquetFile(file_name)
+        df_head = next(file.iter_batches(batch_size=10)).to_pandas()
+        df_iter = file.iter_batches(batch_size=batch_size)
+    else:
+        print("Error only .parquet files allowed.")
+        sys.exit()
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    # Create the table
+    df_head.head(0).to_sql(name=table_name, con=engine, if_exists="replace")
 
-    df = next(df_iter)
+    # Insert values
+    t_start = time()
+    count = 0
+    for batch in df_iter:
+        count += 1
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        batch_df = batch.to_pandas()
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+        print(f"inserting batch {count}...")
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+        b_start = time()
+        batch_df.to_sql(name=table_name, con=engine, if_exists="append")
+        b_end = time()
 
+        print(f"inserted! time taken {b_end-b_start:10.3f} seconds.\n")
 
-    while True: 
-
-        try:
-            t_start = time()
-            
-            df = next(df_iter)
-
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
-            t_end = time()
-
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
-
-        except StopIteration:
-            print("Finished ingesting data into the postgres database")
+        if batch_count and count >= batch_count:
             break
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+    t_end = time()
+    print(
+        f"Completed! Total time taken was {t_end-t_start:10.3f} seconds for {count} batches."
+    )
 
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+
+if __name__ == "__main__":
+    # Parsing arguments
+    parser = argparse.ArgumentParser(
+        description="Loading data from .parquet file link to a Postgres database."
+    )
+
+    parser.add_argument("--user", help="Username for Postgres.")
+    parser.add_argument("--password", help="Password to the username for Postgres.")
+    parser.add_argument("--host", help="Hostname for Postgres.")
+    parser.add_argument("--port", help="Port for Postgres connection.")
+    parser.add_argument("--db", help="Database name for Postgres")
+    parser.add_argument("--tb", help="Destination table name for Postgres.")
+    parser.add_argument("--url", help="URL for .parquet file.")
+    parser.add_argument(
+        "--batch-count", help="number of batch for data loading", type=int
+    )
+    parser.add_argument("--batch-size", help="size of batch for data loading", type=int)
 
     args = parser.parse_args()
-
     main(args)
--- 01-docker-terraform/2_docker_sql/note.md
+++ 01-docker-terraform/2_docker_sql/note.md
@@ -20,6 +20,8 @@ docker run -it \
   -e POSTGRES_DB="ny_taxi" \
   -v d:/05-repo/data-engineering-zoomcamp/01-docker-terraform/2_docker_sql/ny_taxi_postgres_data:/var/lib/postgresql/data \
   -p 5432:5432 \
+  --network=pg-network \
+  --name pg-database \
   postgres:13
 ```
 
@@ -36,5 +38,60 @@ docker run -it \
   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
   -e PGADMIN_DEFAULT_PASSWORD="root" \
   -p 8080:80 \
+  --network=pg-network \
+  --name pgadmin \
   dpage/pgadmin4
+```
+
+- Create a docker network
+
+```bash
+docker network create pg-network
+```
+
+- Convert jupyter to .py script
+
+```bash
+jupyter nbconvert --to=script ingest_data.ipynb
+```
+
+- run ingest_data.py
+
+```bash
+URL="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
+
+python ingest_data.py \
+  --user=root \
+  --password=root \
+  --host=localhost \
+  --port=5432 \
+  --db=ny_taxi \
+  --tb=ny_taxi_data \
+  --url=${URL}
+```
+
+- build and run data ingest docker container
+
+```bash
+docker build -t taxi_ingest:v001 .
+```
+
+```bash
+URL="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
+docker run -it \
+  --network=pg-network \
+  taxi_ingest:v001 \
+  --user=root \
+  --password=root \
+  --host=pg-database \
+  --port=5432 \
+  --db=ny_taxi \
+  --tb=ny_taxi_data \
+  --url=${URL}
+```
+
+# Docker Compose
+
+```bash
+docker-compose up -d
 ```
